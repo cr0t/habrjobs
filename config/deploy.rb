5adcc862aba34c2d4bdfda287680dbb5e19b0fb5
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2010-03-30 12:32:39 +0400

    Fixed deploy config, added javascripts folder with one empty file 'application.js'

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 set :application, "ru.summercode.habrjobs"
-set :repository,  "/var/git/habrjobs.git"
 
 set :scm, :git
 set :branch, "master"
+set :repository, "ssh://summercode.ru/var/git/habrjobs.git"
 
 role :app, "summercode"
 
@@ -13,10 +13,10 @@ set :deploy_to, "/var/www/#{application}"
 
 set :shared_children, %w(tmp)
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
