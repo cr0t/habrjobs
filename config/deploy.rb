34fc9f6e982f7c6286cc1d6fd94476729f1d5018
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-03-18 18:09:38 +0300

    Updated deploy.rb config file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,19 +4,21 @@ set :scm, :git
 set :branch, "master"
 set :repository, "gitosis@git.summercode.com:habrjobs.git"
 
-role :app, "summercode"
+role :app, "habrjobs.summercode.com"
 
 set :user, "cr0t"
 set :use_sudo, false
 
 set :deploy_to, "/var/www/#{application}"
 
-set :shared_children, %w(tmp)
+set :keep_releases, 3
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :restart, :roles => :app, :except => { :no_release => true } do ; end
+  
+  after "deploy:update" do
+    deploy::cleanup
   end
 end
