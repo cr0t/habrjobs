1d392e0b9e51f193552c44c7b37ae864c677c8af
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2011-03-19 00:10:48 +0300

    Added unicorn restart code to deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,9 @@ set :keep_releases, 3
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do ; end
+  task :restart do
+    run "/etc/unicorns/wiki restart"
+  end
   
   after "deploy:update" do
     deploy::cleanup
